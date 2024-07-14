4b30a15cc90351a764d3ec2c5cbd2380328fee44
Wesley Ou <storage.drive.rtrtrtrt@gmail.com>
2024-07-14 16:22:33 -0400

    Remove duplicate code

--- generate_dataset.py
+++ generate_dataset.py
@@ -36,41 +36,6 @@ def generate_images():
 
 # takes the latitude and longitude of a country's capital city and returns a streetview image from
 # a radius around the capital
-import csv
-import os
-import requests
-import pandas as pd
-from PIL import Image
-from io import BytesIO
-import os
-import random
-import requests
-
-# possible countries to label
-country_labels = ["Canada", "United States", "Japan", "Sweden", "France"]
-
-# Base url for google maps api
-base_url = "https://maps.googleapis.com/maps/api/streetview?"
-
-# base directory
-base_dir = os.path.dirname(os.path.dirname(__name__))
-
-# map api key
-maps_api_key = open("api_key.txt", "r").read()
-
-def generate_images():
-    with open('countries.csv') as countries_csv:
-        heading = next(countries_csv)
-        countries_csv_reader = csv.reader(countries_csv)
-        for row in countries_csv_reader:
-            country_name = str(row[0])
-            country_capital_lat = float(row[1])
-            country_capital_lng = float(row[2])
-            for i in range(0, 10):
-                get_image(i, country_name, country_capital_lat, country_capital_lng)
-
-# takes the latitude and longitude of a country's capital city and returns a streetview image from
-# a radius around the capital
 def get_image(id: int, country_name: str, capital_latitude: float, capital_longitude: float):
     for _ in range(5): # try 5 different locations
         random_lat_around_capital = random.random() * ((capital_latitude - 0.1) - (capital_latitude + 0.1)) + (
